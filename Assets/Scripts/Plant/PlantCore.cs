49c395a03d10315dac0620770ff806311943b1d0
Sebastian K Øie <sebagnus@gmail.com>
2022-03-06 18:12:36 +0100

    Plant growth now exist + Milestone registration
    Plant now grows at GrowthRate in GrowthSpeed.
    
    Plants check if they have reached milestone (Later this will stop water usage + growth to give milestone task).
    
    Plant milestone sprite change is there but we do not have enough art for this yet,

--- Assets/Scripts/Plant/PlantCore.cs
+++ Assets/Scripts/Plant/PlantCore.cs
@@ -9,33 +9,38 @@ public class PlantCore : MonoBehaviour
     public PlantDesigner plantTemplate;
 
     [Header("Plant Looks")]
-    [Tooltip("The name the plant will go by")] private string _nameOfPlant; //Simple now, could future work add list of potential names to choose from instead.
+    [Tooltip("The name the plant will go by")] private string nameOfPlant; //Simple now, could future work add list of potential names to choose from instead.
 
     //In the scenario plants had different amount of stages, this would be a list instead of 4 separate sprites.
     [Tooltip("The sprite that represent the seed form of the plant")] private Sprite _seed;
-    [Tooltip("The sprite that represent the first growth stage of the plant")] private Sprite _growthStage1;
-    [Tooltip("The sprite that represent the second growth stage of the plant")] private Sprite _growthStage2;
-    [Tooltip("The sprite that represent the fully grown plant")] private Sprite _fullyGrown;
+    [Tooltip("The sprite that represent the first growth stage of the plant")] private Sprite growthStage1;
+    [Tooltip("The sprite that represent the second growth stage of the plant")] private Sprite growthStage2;
+    [Tooltip("The sprite that represent the fully grown plant")] private Sprite fullyGrown;
 
     [Header("Plant Stats")]
-    [Tooltip("The 3 values the plant need to reach for i'ts progression")] private int[] _progressionCostOfPlant = new int[3]; //Gives a list of 3 ints, a range can be added(need to know the wanted values)
-    [Tooltip("The tasks that the plant preffers, and will ask for")] private string[] _preferencesOfPlant; //Gives a list to implement the preffered tasks, need to know what form the tasks will arrive in before it can be easier to use.
+    [Tooltip("The 3 values the plant need to reach for i'ts progression")] private int[] ListprogressionCostOfPlant = new int[3]; //Gives a list of 3 ints, a range can be added(need to know the wanted values)
+    [Tooltip("The tasks that the plant preffers, and will ask for")] private string[] preferencesOfPlant; //Gives a list to implement the preffered tasks, need to know what form the tasks will arrive in before it can be easier to use.
 
-    [Tooltip("The price of the plant when it can be aquired")] private int _buyingPriceOfPlant; //Simple int for the price of buyig the plant
-    [Tooltip("The price of the plant when it is fully grown and is to be sold")] private int _sellingPriceOfPlant; //Simple int for the price of selling the plant
+    [Tooltip("The price of the plant when it can be aquired")] private int buyingPriceOfPlant; //Simple int for the price of buyig the plant
+    [Tooltip("The price of the plant when it is fully grown and is to be sold")] private int sellingPriceOfPlant; //Simple int for the price of selling the plant
     #endregion
     #region Other Variables
-    private SpriteRenderer _spriteOfPlant; //Refrence to the plants spriterenderer.
+    private SpriteRenderer spriteOfPlant; //Refrence to the plants spriterenderer.
 
-    private int _growthStageAmount = 3; //Sets a definit value on the amount of stages a plant transforms into. Seed is the base and turns into = (stage 1, stage 2 & Fully grown)
+    private int growthStageAmount = 3; //Sets a definit value on the amount of stages a plant transforms into. Seed is the base and turns into = (stage 1, stage 2 & Fully grown)
 
-    [SerializeField] private int _currentPlantProgressionValue; //!!Serialized for Testing!! The value of the plants current progression. (can be used to determine growth stage) 
-    private int _growthStage; //The value representation of the current stage (0 = seed, 3 = fully grown)
+    [SerializeField] private int currentGrowthValue; //!!Serialized for Testing!! The value of the plants current progression. (can be used to determine growth stage) 
+    [SerializeField] private int growthRate = 1; //The amount it tic grows.
+    [SerializeField] private float growthSpeed = 2f; //The speed it tic grows
 
-    private float waterStoredInPlant; //The amount of water this plant has been given(Lowers over time)
-    private float waterPlantCanStoreLimit = 50; //The upper amount of wate this plant can store.
+    private int growthStage; //The value representation of the current stage (0 = seed, 3 = fully grown)
+
+    private int waterStoredInPlant; //The amount of water this plant has been given(Lowers over time)
+
+    private float waterPlantCanStoreLimit = 50; //The upper amount of wate this plant can store. This should be changeable?
+
     [HideInInspector] public bool WateringIsInProgress; //Is the watering can being held?
-    private float wateringTickSpeed = 1f; //How fast will the watering take place.
+    private float wateringTickSpeed = 0.5f; //How fast will the watering take place.
     private float wateringTimer; //Timer to limit water speed.
     #endregion
 
@@ -67,6 +72,9 @@ public class PlantCore : MonoBehaviour
             wateringTimer = 0; //Resets timer.
             //Debug.Log("Button was released");
         }
+
+        GrowingPlant(); //Growing power of plant!
+        CheckForMilestone(); //Checks if milestones have been reached (Robust for Prototype)
     }
     #endregion
 
@@ -137,51 +145,94 @@ public class PlantCore : MonoBehaviour
     }
 
     #endregion
+    #region Plant Growth
+    private void GrowingPlant()
+    {
+        if(waterStoredInPlant > 0) //Only takes effect if there is water!!!
+        {
+            StartCoroutine(PlantGowing());
+        }
+    }
+    
+    IEnumerator PlantGowing()
+    {
+        yield return new WaitForSeconds(growthSpeed);
+        if (waterStoredInPlant > 0)
+        {
+            --waterStoredInPlant;
+            currentGrowthValue += growthRate; //Increases progression, add modifiers for growth rate later?
+            StartCoroutine(PlantGowing()); //Reruns the program.
+        }
+        else
+        {
+            Debug.Log(gameObject.name + " ran out of water ;(");
+        }
+    }
+
+    #endregion
+    #region Milestone Reach
+    private void CheckForMilestone()
+    {
+        if(currentGrowthValue >= ListprogressionCostOfPlant[2]) //Checks if the progression has reached the laststage.
+        {
+            if(growthStage != 3) //Checks that it has not already changed stage
+            {
+            growthStage = 3; //Sets the growthStage to Full grown
+            PlantSpriteChange(); //Update sprite to the new plant form.
+            }
+        }
+        else if(currentGrowthValue >= ListprogressionCostOfPlant[1]) //Check if the progression has reached the second stage.
+        {
+            if(growthStage != 2) //Checks if it has not already changed stage
+            {
+            growthStage = 2; //Sets the growthStage to stage 2
+            PlantSpriteChange(); //Update sprite to the new plant form.
+            }
+        }
+        else if(currentGrowthValue >= ListprogressionCostOfPlant[0]) //Check if the progression has reached the second stage.
+        {
+            if(growthStage != 1) //Checks if it has not already changed stage
+            {
+            growthStage = 1; //Sets the growthStage to stage 1
+            PlantSpriteChange(); //Update sprite to the new plant form.
+            }
+        }
+    }
+    #endregion
     #region Sprite changing
     private void PlantSpriteChange()
     {
-        _spriteOfPlant = GetComponent<SpriteRenderer>(); //Makes sure the spriterenderer being used is the gameobjects own.
+        spriteOfPlant = GetComponent<SpriteRenderer>(); //Makes sure the spriterenderer being used is the gameobjects own.
 
-        Sprite[] growthStages = new Sprite[] { _growthStage1, _growthStage2, _fullyGrown }; //Creates a list of all the growth stages. (Hardcoded for now)
+        Sprite[] growthStages = new Sprite[] { growthStage1, growthStage2, fullyGrown }; //Creates a list of all the growth stages. (Hardcoded for now)
 
-        for (int stage = 0; stage < _growthStageAmount; stage++) //Loop to go through all the 3 stage increase values to check for growth.
+        if(growthStage != 0)
         {
-            if(_progressionCostOfPlant[stage] <= _currentPlantProgressionValue) //If the plant has reached this stage, do it.
-            {
-                _spriteOfPlant.sprite = growthStages[stage]; //Since the plant has reached the stage, then change the sprite to the appropriate stage. 
-
-
-                _growthStage = stage + 1; //sets the growth stage of the plant to it's appropriate value (Later will be a saved value? Prevent unlimited milestones.) !!!Move location late?
-            }
-            else if(_spriteOfPlant.sprite == null) //If it has not been given a sprite, make it a seed. (Takes effect if it has not reached stage 1 one)
-            {
-                _spriteOfPlant.sprite = _seed; //Sprite is now a seed.
-            }
+            spriteOfPlant.sprite = growthStages[growthStage]; //Since the plant has reached the stage, then change the sprite to the appropriate stage. 
+        }
+        else
+        {
+            spriteOfPlant.sprite = _seed; //Sprite is now a seed.
         }
 
-        Debug.Log("Current stage is = " + _growthStage + "and current sprite image is = " + _spriteOfPlant.sprite.name);
+        Debug.Log("Current stage is = " + growthStage + "and current sprite image is = " + spriteOfPlant.sprite.name);
     }
     #endregion
     #region Setting up plant
     private void PlantInfoFeed() //To be run first. Updates the variables of the script to fit the given template
     {
-        this._nameOfPlant = plantTemplate.nameOfPlant; //Update the name to be the one from the template.
+        this.nameOfPlant = plantTemplate.nameOfPlant; //Update the name to be the one from the template.
 
         this._seed = plantTemplate.seed; //Update the seed sprite to be the one from the template.
-        this._growthStage1 = plantTemplate.growthStage1; //Update the growth 1 sprite to be the one from the template.
-        this._growthStage2 = plantTemplate.growthStage2; //Update the growth 2 sprite to be the one from the template.
-        this._fullyGrown = plantTemplate.fullyGrown; //Update the fully grown sprite to be the one from the template.
+        this.growthStage1 = plantTemplate.growthStage1; //Update the growth 1 sprite to be the one from the template.
+        this.growthStage2 = plantTemplate.growthStage2; //Update the growth 2 sprite to be the one from the template.
+        this.fullyGrown = plantTemplate.fullyGrown; //Update the fully grown sprite to be the one from the template.
 
-        this._progressionCostOfPlant = plantTemplate.progressionCostOfPlant; //Update the progression costs to be the ones from the template.
-        this._preferencesOfPlant = plantTemplate.preferencesOfPlant; //Update the preferences to be the ones from the template.
+        this.ListprogressionCostOfPlant = plantTemplate.progressionCostOfPlant; //Update the progression costs to be the ones from the template.
+        this.preferencesOfPlant = plantTemplate.preferencesOfPlant; //Update the preferences to be the ones from the template.
 
-        this._buyingPriceOfPlant = plantTemplate.buyingPriceOfPlant; //Update the buy price to be the one from the template.
-        this._sellingPriceOfPlant = plantTemplate.sellingPriceOfPlant; //Update the sell price to be the one from the template.
+        this.buyingPriceOfPlant = plantTemplate.buyingPriceOfPlant; //Update the buy price to be the one from the template.
+        this.sellingPriceOfPlant = plantTemplate.sellingPriceOfPlant; //Update the sell price to be the one from the template.
     }
     #endregion
-
-    public void testButton(GameObject tester)
-    {
-
-    }
 }

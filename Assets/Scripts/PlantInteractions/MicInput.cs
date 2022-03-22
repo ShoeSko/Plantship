419a28620e49369507ac21bd0bb29e777e039f69
Sebastian K Øie <sebagnus@gmail.com>
2022-03-22 22:29:40 +0100

    Microphone Volume registration work
    Started trying to understand how to properly register volume of microphone.

---
+++ Assets/Scripts/PlantInteractions/MicInput.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicInput : MonoBehaviour
+{
+
+    public float testSound;
+    public static float MicLoudness;
+    private string _device;
+    private AudioClip _clipRecord;
+    private int _sampleWindow = 128;
+    private bool _isInitialized;
+
+    void InitMic()
+    {
+        if (_device == null)
+        {
+            _device = Microphone.devices[0];
+            _device = null;
+            _clipRecord = Microphone.Start(_device, true, 999, 44100);
+            Debug.Log(_clipRecord);
+        }
+    }
+
+    void StopMicrophone()
+    {
+        Microphone.End(_device);
+    }
+
+    float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(_device) - (_sampleWindow + 1);
+        if (micPosition < 0)
+        {
+            return 0;
+        }
+        _clipRecord.GetData(waveData, micPosition);
+        for (int i = 0; i < _sampleWindow; ++i)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+    void Update()
+    {
+        MicLoudness = LevelMax();
+        testSound = MicLoudness;
+
+    }
+
+    void OnEnable()
+    {
+        InitMic();
+        _isInitialized = true;
+    }
+
+    void OnDisable()
+    {
+        StopMicrophone();
+    }
+
+    void OnDestory()
+    {
+        StopMicrophone();
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            if (!_isInitialized)
+            {
+                InitMic();
+                _isInitialized = true;
+            }
+        }
+
+        if (!focus)
+        {
+            StopMicrophone();
+            _isInitialized = false;
+        }
+    }
+
+}
--- Assets/Scripts/PlantInteractions/MicrophoneTester.cs
+++ Assets/Scripts/PlantInteractions/MicrophoneTester.cs
@@ -12,9 +12,13 @@ public class MicrophoneTester : MonoBehaviour
         for (int Mic = 0; Mic < Microphone.devices.Length; Mic++)
         {
             listOfDevices.Add(Microphone.devices[Mic]);
-            Debug.Log("Microphone option " + Mic + " is called " + listOfDevices[Mic]);
+            //Debug.Log("Microphone option " + Mic + " is called " + listOfDevices[Mic]);
             textDisplay[Mic].text = "Mic #" + (Mic+1) + " is called " + listOfDevices[Mic];
         }
+
+
+        Microphone.Start(null, false, 5, 44100);
+
 
         //Debug.Log("Mic 1 is " + Microphone.devices[0] + " and the amount of Mics is " + Microphone.devices.Length);
     }
@@ -22,6 +26,7 @@ public class MicrophoneTester : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(Microphone.GetPosition(null));
+        Debug.Log(Microphone.IsRecording(null));
     }
 }
